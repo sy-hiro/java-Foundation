318a97d5e7489cf9e633473bd2c6b46f6d45efcd
sy-hiro <oneok.yukki127@gmail.com>
2024-05-01 12:12:52 +0900

    ファイル全ての取り込み

---
+++ src/jp/kadai/laerningjava/Kadai.java
@@ -0,0 +1,11 @@
+package jp.kadai.laerningjava;
+
+public class Kadai {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello Eclipse\n\'It`s Tuesday today.\'\n\"今日は火曜日です。\"");
+
+	}
+
+}
